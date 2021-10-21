0ed508f679f81284be43e9745ea719eb2197f216
biernat <Manimo12#git>
2021-10-21 17:35:30 +0200

    added no UI & external (WIP)

---
+++ samples/no-ui/no-ui.js
@@ -0,0 +1,25 @@
+function switchImageSample() {
+  for (var i = 1; i < 4; i++){
+    var el = document.getElementById("img" + i);
+    $FSI.addEvent(el, "click", onThumbnailClick);
+  }
+}
+
+function onThumbnailClick(){
+
+  var img = this.getElementsByTagName("img");
+
+  if (img && img[0]){
+
+    // get the src attribute
+    var strImageURL = img[0].getAttribute("src") ;
+
+    // get the "source" parameter value
+    var src = $FSI.utils.getParameterValueFromURL(strImageURL, 'source');
+
+    // change the image in FSI Viewer
+    var parameters = {"imagePath" : src};
+    var viewer = document.getElementById("image");
+    viewer.changeImage(parameters)
+  }
+}
