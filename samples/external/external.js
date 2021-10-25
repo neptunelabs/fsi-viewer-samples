96695c07fc22409f9e9aaee808826ccad70755d8
biernat <Manimo12#git>
2021-10-25 17:45:07 +0200

    working on no UI & external (WIP)

--- samples/external/external.js
+++ samples/external/external.js
@@ -1,4 +1,30 @@
-  function initSlider() {
+function switchImageSample() {
+  for (var i = 1; i < 4; i++){
+    var el = document.getElementById("img" + i);
+    $FSI.addEvent(el, "click", onThumbnailClick);
+  }
+}
+
+function onThumbnailClick(){
+
+  var img = this.getElementsByTagName("img");
+
+  if (img && img[0]){
+
+    // get the src attribute
+    var strImageURL = img[0].getAttribute("src") ;
+
+    // get the "source" parameter value
+    var src = $FSI.utils.getParameterValueFromURL(strImageURL, 'source');
+
+    // change the image in FSI Viewer
+    var parameters = {"imagePath" : src};
+    var viewer = document.getElementById("image");
+    viewer.changeImage(parameters)
+  }
+}
+
+function initSlider() {
   new ZoomSliderControl(
     document.getElementById('image'),
     document.getElementById('js-zoomslider')
