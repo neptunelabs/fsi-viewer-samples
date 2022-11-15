99f7daad39d6250a6a15bb04b000a4793e805a62
Diana <biernat@neptunelabs.com>
2022-11-15 13:39:35 +0100

    changed images

--- samples/modal/modal.js
+++ samples/modal/modal.js
@@ -4,20 +4,16 @@ function changeImage(buttonID) {
   let curImage = document.getElementById('image');
   switch (buttonID) {
     case "0":
-      img =  '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg&width=940';
-      src = 'images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg';
+      img =  '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/architect-2071534.jpg&rect=0.16764,0,0.66471,1&width=840';
+      src = 'images/samples/ssi/furniture/architect-2071534.jpg';
       break
     case "1":
-      img = '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-jSbBnR22cXc-unsplash.jpg&width=940&rect=0,0.29634,1,0.66709';
-      src = 'images/samples/ssi/furniture/nathan-oakley-jSbBnR22cXc-unsplash.jpg';
-      break
-    case "2":
-      img = '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-CZysNmnvEJg-unsplash.jpg&width=940';
-      src = 'images/samples/ssi/furniture/nathan-oakley-CZysNmnvEJg-unsplash.jpg';
+      img = '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/architecture-2050197.jpg&rect=0.18441,0,0.66471,1&width=840';
+      src = 'images/samples/ssi/furniture/architecture-2050197.jpg';
       break
     default:
-      img = '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg&width=940';
-      src = 'images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg';
+      img = '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/architect-2071534.jpg&rect=0.16764,0,0.66471,1&width=840';
+      src = 'images/samples/ssi/furniture/architect-2071534.jpg';
   }
   curImage.src = img;
   document.getElementsByTagName("fsi-viewer")[0].changeConfig(undefined, {imagesrc: src});
