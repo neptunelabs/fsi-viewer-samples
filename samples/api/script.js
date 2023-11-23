d2f90b6958cb49fbd5ac92aa41818ab7e6611e82
nelabiernat <60930527+nelabiernat@users.noreply.github.com>
2023-11-23 10:08:36 +0100

    added functions again

--- samples/api/script.js
+++ samples/api/script.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  let instance
 
   document.getElementById("zoomBtn").addEventListener("click", () => {
 
-    const instance = new $FSI.Viewer('zoomEle',{
+    instance = new $FSI.Viewer('zoomEle',{
       src: 'images/samples/Shoe/View2/sneaker-both-13.jpg',
       debug: true,
       plugins: 'resize,fullScreen',
@@ -12,13 +13,25 @@ document.addEventListener("DOMContentLoaded", function() {
       height: '427',
       onReady: () => {
         hideImg()
-      },});
+      },
+      onAnimationComplete: () => {
+        setTimeout(changeZoom(), 500);
+      }});
     instance.start();
 
     function hideImg () {
       document.getElementById("zoomEle").style.visibility = "visible";
       document.getElementById("zoomImg").style.display = "none";
       document.getElementById("zoomBtn").style.display = "none";
+    }
+
+    function changeZoom () {
+      instance.setZoom('50', true, true)
+      setTimeout(resetZoom, 500);
+    }
+
+    function resetZoom () {
+      instance.resetView()
     }
   });
 
