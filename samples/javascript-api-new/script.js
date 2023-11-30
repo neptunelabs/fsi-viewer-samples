82c78a17d1aeef8148bf8d19e1392f0bf92c6c76
nelabiernat <60930527+nelabiernat@users.noreply.github.com>
2023-11-30 11:52:52 +0100

    testing new $FSI.createNode

--- samples/javascript-api-new/script.js
+++ samples/javascript-api-new/script.js
@@ -4,43 +4,46 @@ document.addEventListener('DOMContentLoaded', () => {
     let showTeaser = true
     let teaserZoomPercent = 10
 
+    function showViewer() {
+      console.log('showViewer should happen now?')
+      // show FSI Viewer instance and hide image
+      document.getElementById('zoomEle').style.visibility = 'visible'
+      document.getElementById('zoomImg').style.display = 'none'
+      document.getElementById('zoomBtn').style.display = 'none'
+      if (showTeaser) {
+        setTimeout(() => {
+          instance.setZoom(teaserZoomPercent, true, true)
+        }, 500)
+      }
+    }
+
+    function viewChange() {
+      console.log('viewChange should happen now?')
+      if (showTeaser) {
+        showTeaser = false
+        setTimeout(() => {
+          // reset viewer - the user can interact with the UI
+          instance.resetView()
+        }, 800)
+      }
+    }
+
     const container = document.getElementById('zoomEle');
+    const instance = $FSI.createNode("fsi-viewer", {
+      src: 'images/samples/Shoe/View2/sneaker-both-13.jpg',
+      debug: false,
+      plugins: 'fullScreen',
+      skin: 'example',
+      width: '640',
+      height: '427',
+      id : 'myViewer'
+ })
+    container.appendChild(instance)
 
-    container.appendChild(
-      $FSI.createNode("fsi-viewer", {
-        src: 'images/samples/Shoe/View2/sneaker-both-13.jpg',
-        debug: false,
-        plugins: 'fullScreen',
-        skin: 'example',
-        width: '640',
-        height: '427',
-        id: 'myViewer',
-        // listen for finished loading FSI Viewer and becomes interactive
-        onReady: () => {
-          // show FSI Viewer instance and hide image
-          document.getElementById('zoomEle').style.visibility = 'visible'
-          document.getElementById('zoomImg').style.display = 'none'
-          document.getElementById('zoomBtn').style.display = 'none'
-          if (showTeaser) {
-            setTimeout(() => {
-              document.getElementById('myViewer').setZoom(teaserZoomPercent, true, true)
-            }, 500)
-          }
-        },
-        // listen when zoom is finished
-        onViewChanged: (view) => {
-          if (showTeaser) {
-            showTeaser = false
-            setTimeout(() => {
-              // reset viewer - the user can interact with the UI
-              document.getElementById('myViewer').resetView()
-            }, 800)
-          }
-        },
-      })
-    )
+    instance.start()
 
-    document.getElementById('myViewer').start()
+    instance.addListener("onReady", showViewer);
+    instance.addListener("onViewChanged", viewChange);
   })
 
 })
